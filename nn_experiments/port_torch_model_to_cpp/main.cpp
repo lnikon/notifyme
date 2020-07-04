8eac93e6b076ab582c8b36869baa381129c9686f
lnikon <bejanyan.vahag@protonmail.com>
2020-07-04 17:16:05 +0400

    adding experiments

---
+++ nn_experiments/port_torch_model_to_cpp/main.cpp
@@ -0,0 +1,33 @@
+#include <torch/script.h>
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, char* argv[])
+{	
+	if (argc != 2) {
+		std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+		return -1;
+	}	
+
+
+	torch::jit::script::Module module;
+
+	try {
+		module = torch::jit::load(argv[1]);
+	}
+	catch (const c10::Error& e) {
+		std::cerr << "error loading the model\n";
+		return -1;
+	}
+
+	std::cout << "ok\n";
+
+	std::vector<torch::jit::IValue> inputs;
+	inputs.push_back(torch::ones({1, 3, 244, 244}));
+
+	at::Tensor output = module.forward(inputs).toTensor();
+	std::cout << output.slice(1, 0, 5) << '\n';
+
+	return 0;
+}
